1712b0dba2e0b7a64b55ac32443f1b56fbc77ce2
z2kkwc <78127020+z2kkwc@users.noreply.github.com>
2021-02-03 09:29:20 +0800

    Add files via upload

--- 云函数js目录.js
+++ 云函数js目录.js
@@ -1,3 +1,4 @@
+主函数
 jd_blueCoin&jd_club_lottery&jd_car&jd_car_exchange&jd_baitiao
 0 0 0 * * * *
 jd_redPacket&jd_shop&jd_unsubscribe&jd_unbind&jd_small_home&jd_paopao&jd_bean_sign&jd_sgmh&jd_xgyl
@@ -19,31 +20,44 @@ jd_superMarket&jd_family&jd_nianCollect&jd_wechat_sign&youth&jd_xxl&jd_xxl_gh
 jd_joy_feedPets&jd_joy&jd_moneyTree&jd_plantBean&jd_dreamFactory&jd_jdfactory&jd_immortal_answer
 0 3 * * * * *
 
+备用函数（调用次数少）
+jd_superMarket&jd_nianCollect&jd_wechat_sign&jd_newYearMoney&jd_5g&jd_festival
+0 15 */3 * * * *
+jd_joy_feedPets&jd_joy&jd_moneyTree&jd_plantBean&jd_dreamFactory&jd_jdfactory
+0 3 */3 * * * *
+jd_bean_change&jd_crazy_joy&jd_rankingList&jd_bean_home&jd_car&jd_nh&jd_xg&jd_nian_sign
+0 30 7 * * * *
+jd_nian&jd_immortal&jd_immortal_answer
+0 30 */2 * * * *
+jd_fruit&jd_pet&jd_bookshop&jd_jxnc&jd_ms
+0 5 6-18/6,8 * * * *
+jd_joy_reward&jd_joy_steal&jd_necklace&jd_small_home&jd_live
+0 0 0-16/8,20 * * * *
+jd_pigPet&jd_syj&jd_kd&jd_jdzz&jd_cash&jx_sign&jd_nian_sign&jd_nian_ar&jd_nian_wechat
+0 3 1 * * * *
+jd_redPacket&jd_shop&jd_unsubscribe&jd_unbind&jd_small_home&jd_paopao&jd_bean_sign&jd_sgmh
+0 10 6 * * * *
+jd_blueCoin&jd_club_lottery&jd_car&jd_car_exchange&jd_baitiao
+0 0 0 * * * *
 
-	
+
 AAA=BBB
-JD_COOKIE= 京东ck
+JD_COOKIE= xxx
 DDFACTORY_SHARECODES=T023v_13RRYR8VTWJwfznf4IfeQCjVWnYaS5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWnYaS5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWnYaS5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWnYaS5kRrbA
-DREAM_FACTORY_SHARE_CODES=wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=&wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=&wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=&wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0= FRUITSHARECODES=b47bee19f0fc49ca867f6061b39a573d&b47bee19f0fc49ca867f6061b39a573d&b47bee19f0fc49ca867f6061b39a573d&b47bee19f0fc49ca867f6061b39a573d
+DREAM_FACTORY_SHARE_CODES=wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=&wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=&wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=&wghMdDVJNcHKBp8YYQWuUp0WJSbpHcLYCpXyA8LA-g0=
 FRUITSHARECODES=b47bee19f0fc49ca867f6061b39a573d&b47bee19f0fc49ca867f6061b39a573d&b47bee19f0fc49ca867f6061b39a573d&b47bee19f0fc49ca867f6061b39a573d
-JDJOY_APPLYJDBEAN=2000
+JD818_SHARECODES=aa36c33d-9311-41d7-bb2d-0435c4e84fc7&aa36c33d-9311-41d7-bb2d-0435c4e84fc7&aa36c33d-9311-41d7-bb2d-0435c4e84fc7&aa36c33d-9311-41d7-bb2d-0435c4e84fc7
 JDJOY_SHARECODES=s0okzL0oyJuOiVrEow3Ko79A94gcAdrU&s0okzL0oyJuOiVrEow3Ko79A94gcAdrU&s0okzL0oyJuOiVrEow3Ko79A94gcAdrU&s0okzL0oyJuOiVrEow3Ko79A94gcAdrU&s0okzL0oyJuOiVrEow3Ko79A94gcAdrU
-JDNIANPK_SHARECODES=IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCs&IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCs&IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCs&IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCs
+JDNIANPK_SHARECODES=IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCw&IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCw&IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCw&IgNWdiLGaPbb6grMAA2t72aml9Oq6je-0uqgGoGXlxoyck8ApW9Ldsx2rh0dpItHCCw
 JDNIAN_SHARECODES=cgxZLmGGIbLS7g7IDxiu5gDlEbI47IzgJi9N78jFDKtfrjowJybqv6vj8448rg&cgxZLmGGIbLS7g7IDxiu5gDlEbI47IzgJi9N78jFDKtfrjowJybqv6vj8448rg&cgxZLmGGIbLS7g7IDxiu5gDlEbI47IzgJi9N78jFDKtfrjowJybqv6vj8448rg&cgxZLmGGIbLS7g7IDxiu5gDlEbI47IzgJi9N78jFDKtfrjowJybqv6vj8448rg
-JDSGMH_SHARECODES=T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA JDSXSY_SHARECODES=44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc&44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc&44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc&44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc
+JDNY_SHARECODES=-5I1Xu5ApItWBP82Prly9uftYBS0Jp4h7CQco2QVYViadrUSxA&-5I1Xu5ApItWBP82Prly9uftYBS0Jp4h7CQco2QVYViadrUSxA&-5I1Xu5ApItWBP82Prly9uftYBS0Jp4h7CQco2QVYViadrUSxA&-5I1Xu5ApItWBP82Prly9uftYBS0Jp4h7CQco2QVYViadrUSxA
+JDSGMH_SHARECODES=T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA&T023v_13RRYR8VTWJwfznf4IfeQCjVWmIaW5kRrbA
 JDSXSY_SHARECODES=44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc&44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc&44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc&44xIs4YwE5Z7HhZ_xiV0NKN1rOoNlEqBvKERBHL-Acl-z0GKsQWw0xbiz51yIDe72mc5qc
-JD_BEAN_SIGN_NOTIFY_SIMPLE=true
 JD_CASH_SHARECODES=IhszaeS6Yvwn9nPSwnoR3zM&IhszaeS6Yvwn9nPSwnoR3zM&IhszaeS6Yvwn9nPSwnoR3zM&IhszaeS6Yvwn9nPSwnoR3zM
-JD_JOY_REWARD_NAME=20
-JXNC_SHARECODES={"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}&{"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}&{"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}&{"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1} MARKET_REWARD_NOTIFY=false
-MARKET_REWARD_NOTIFY=false
+JSMOBILEFESTIVAL_SHARECODES=c2b24b13-24f2-4a88-966d-59b4f9573f88&c2b24b13-24f2-4a88-966d-59b4f9573f88&c2b24b13-24f2-4a88-966d-59b4f9573f88&c2b24b13-24f2-4a88-966d-59b4f9573f88
+JXNC_SHARECODES={"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}&{"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}&{"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}&{"smp":"09f5757fff09a16cf42efb16652279c4","active":"jdnc_1_penggan210123_2","joinnum":1}
 PETSHARECODES=MTE1NDUwMTI0MDAwMDAwMDQyOTgxNjU3&MTE1NDUwMTI0MDAwMDAwMDQyOTgxNjU3&MTE1NDUwMTI0MDAwMDAwMDQyOTgxNjU3&MTE1NDUwMTI0MDAwMDAwMDQyOTgxNjU3
 PLANT_BEAN_SHARECODES=4a4p77hwqfa47asfy5sscs44lmeroko5ztal7oa&4a4p77hwqfa47asfy5sscs44lmeroko5ztal7oa&4a4p77hwqfa47asfy5sscs44lmeroko5ztal7oa&4a4p77hwqfa47asfy5sscs44lmeroko5ztal7oa
-SUPERMARKET_LOTTERY=true
-UN_BIND_CARD_NUM=0
-
-
-
 
 
 
